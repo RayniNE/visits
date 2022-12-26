8328f0c8be4a3109e9dd33ffc00f888f2a3764a5
RayniNE <timetopwm@gmail.com>
2022-12-26 18:50:07 -0300

    Added the docker-compose file with the corresponding structure for the service to work.

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ const express = require("express");
 const redis = require("redis");
 
 const app = express();
-const client = redis.createClient();
+const client = redis.createClient({
+  // ? while using docker-compose, we just need to set the service name as the host,
+  // ? and docker will automatically match it
+  // ? to the corresponding container.
+  host: "redis-server",
+  // ? this is the default port that redis use, adding it for clarification
+  port: 6379,
+});
 
 // Set the initial visits to 0.
 client.set("visits", 0);
